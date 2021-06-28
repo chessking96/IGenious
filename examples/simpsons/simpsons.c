736785b5bd234f4e084ea799fc1711826e7684b4
Jakob Roffler <jroffler@student.ethz.ch>
2021-06-28 16:38:19 +0200

    changed simpsons a little bit

--- examples/simpsons/simpsons.c
+++ examples/simpsons/simpsons.c
@@ -1,5 +1,7 @@
+#include "math.h"
+
 long double fun(long double x) {
-  long double pi = 3.1415L; // was acos(-1)
+  long double pi = 3.14159265358979323846L; // was acos(-1)
   long double result;
   result = pi * x - pi * pi * pi * x * x * x / 6 + pi * pi * pi * pi * pi * x * x * x * x * x / 120; // was sin(pi * x)
   return result;
@@ -15,22 +17,23 @@ long double simpsons() {
 
   int i, j, k; // diff: added constants
   int n = 1000000; // was n=1000000
-  long double a, b;
+  long double a;
+  long double b;
   long double h, s1, x;
 
-    a = 0.0;
-    b = 1.0;
-    h = (b - a) / (2.0 * n);
-    s1= 0.0;
+    a = 0.0L;
+    b = 1.0L;
+    h = (b - a) / (2.0L * n);
+    s1 = 0.0L;
 
     x = a;
     s1 = fun(x);
 
     for(i = 0; i < n; i++){
       x = x + h;
-      s1 = s1 + 4.0 * fun(x);
+      s1 = s1 + 4.0L * fun(x);
       x = x + h;
-      s1 = s1 + 2.0 * fun(x);
+      s1 = s1 + 2.0L * fun(x);
     }
 
 
