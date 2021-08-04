6659ae362867910ccac2273139959b56cd486190
Jakob Roffler <jroffler@student.ethz.ch>
2021-08-04 08:04:42 +0200

    fixed some plot issues

--- benchmarks/clean.py
+++ benchmarks/clean.py
@@ -5,4 +5,5 @@ from helper import call
 folders = ['bisection_root', 'DFT16', 'DFT16dd', 'dot', 'funarc', 'linear', 'matmul', 'newton_root', 'simpsons']
 
 for folder in folders:
-    call('cd examples/' + folder + ' && find . -name "non*" -exec rm -rf {} \;')
+    call('cd examples/' + folder + ' && find . -name "analysis*" -exec rm -rf {} \;')
+    call('cd examples/' + folder + ' && find . -name "config*" -exec rm -rf {} \;')
