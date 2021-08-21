35dc25d15b472db4cce9c378822aaa988436d360
Jakob Roffler <jroffler@student.ethz.ch>
2021-08-21 11:59:09 +0200

    added some backup files

---
+++ old/changeTypes_nonmixed.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys, os, json
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from helper import call, getEnvVar, call_background, load_json
+
+def run(file_name, folder, config_name):
+
+    scripts_path = getEnvVar('SOURCE_PATH') + '/scripts'
+    igen_src = getEnvVar('IGEN_PATH')
+
+    # Make sure, that the clang_ast_vistor latest version is compiled
+    call_background('cd ' + scripts_path + '/changeTypes && cmake . && make')
+
+    # Run clang_ast_visitor
+    call_background('cd examples/' + folder + '/analysis_' + config_name + '/precimonious_setup && ' + scripts_path + '/changeTypes/clang_ast_visitor ../igen_setup/rmd_' + file_name + ' -- ' + ' >  ../igen_setup/chg_rmd_' + file_name)
+
+    # Call IGen
+    call_background('python3 ' + igen_src + '/bin/igen.py examples/' + folder + '/analysis_' + config_name + '/igen_setup/chg_rmd_' + file_name)
